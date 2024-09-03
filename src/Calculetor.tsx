01391f11a2e25d30e906d817ec33db1d9d4c7ae7
Anuj_dev <anukumar@zuelligpharma.com>
2024-09-03 15:28:59 +0530

    '1st commit'

---
+++ src/Calculetor.tsx
@@ -0,0 +1,67 @@
+import React, {useState} from 'react';
+import './Calculetor.css';
+
+const Calculetor = () => {
+    const [input,setInput] = useState<string>('');
+    const [result, setResult] = useState<string | number>(0);
+
+    let btnSet1 = ['1','2','3','+'];
+    let btnSet2 = ['4','5','6','-'];
+    let btnSet3 = ['7','8','9','*'];
+    let btnSet4 = ['0','.','/','='];
+
+    const handleClick = (btn:string) => {
+        if(btn === '=') {
+            try {
+                setResult(eval(input));
+            } catch (error) {
+                setResult('error');
+            }
+        }
+        else if(btn === 'c1') {
+            setInput(input.slice(0,-1));
+            if(result) {
+                setResult(0);
+            }
+        }
+        else if(btn === 'c') {
+            setInput('');
+            setResult(0);
+        }
+        else {
+            setInput(input + btn);
+            console.log('input field ===>',input);       
+        }
+    }
+    return(
+        <>
+         <div className='calculetor'>
+            <div className='displayField'>
+                <input type="text" value={input} />
+                <div className='result'>{result}</div>
+            </div>
+            <div className='buttonKey'>
+                {btnSet1.map((btn) => (
+                    <button key={btn} onClick={() => handleClick(btn)}>{btn}</button>
+                ))}
+
+                {btnSet2.map((btn) => (
+                    <button key={btn} onClick={() => handleClick(btn)}>{btn}</button>
+                ))}
+
+                {btnSet3.map((btn) => (
+                    <button key={btn} onClick={() => handleClick(btn)}>{btn}</button>
+                ))}
+
+                {btnSet4.map((btn) => (
+                    <button key={btn} onClick={() => handleClick(btn)}>{btn}</button>
+                ))}
+                <button key={'c'} onClick={() => handleClick('c')}>C</button>
+                <button key={''} onClick={() => handleClick('c1')}>R</button>
+            </div>
+         </div>
+        </>
+    )
+}
+
+export default Calculetor;
